9cc7838aeef7ebaa18ba7c8d34f359a08cad73e1
Timur Badretdinov <accounts@destiner.io>
2024-11-22 19:04:14 +0100

    format

--- apps/next/wagmi.ts
+++ apps/next/wagmi.ts
@@ -2,16 +2,16 @@ import { cookieStorage, createStorage } from '@wagmi/core'
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
 import { base } from '@reown/appkit/networks'
 
-export const projectId = '9df3dd72ed4409e550edabebd77bb221';
+export const projectId = '9df3dd72ed4409e550edabebd77bb221'
 export const networks = [base]
 
 export const wagmiAdapter = new WagmiAdapter({
   storage: createStorage({
-    storage: cookieStorage
+    storage: cookieStorage,
   }),
   ssr: true,
   projectId,
-  networks
+  networks,
 })
 
 export const config = wagmiAdapter.wagmiConfig
